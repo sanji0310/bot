8ac8e5ebacff33c1955b47b91a473e51563a4d4e
GlooSmoke <147390858+GlooSmoke@users.noreply.github.com>
2024-04-18 13:21:11 +0700

    Update mchk.php

--- gates/mchk.php
+++ gates/mchk.php
@@ -21,7 +21,7 @@ $stchk = "<b>Started Checking...</b>";
     $aray = gibarray($message);
     $cout = count($aray);
     $total = $cout * 1;
-    if (count($aray) > 5){
+    if (count($aray) > 500){
   $cmsg = "𝗧𝗛𝗜𝗦 𝗚𝗔𝗧𝗘 𝗜𝗦 𝗟𝗜𝗠𝗜𝗧𝗘𝗗 𝗧𝗢 𝗖𝗛𝗘𝗖𝗞 𝗙𝗢𝗥 𝟱 𝗖𝗖 𝗢𝗡𝗟𝗬 ⚠️";
   editMessage($chatId,$cmsg,$mes_id);
     exit();
