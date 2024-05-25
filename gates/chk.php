1474c92839591e8f7b3ac6573661bab79aa5522b
GlooSmoke <147390858+GlooSmoke@users.noreply.github.com>
2024-05-25 16:06:44 +0700

    Update chk.php

--- gates/chk.php
+++ gates/chk.php
@@ -1,5 +1,59 @@
 <?php
+//=========RANK DETERMINE=========//
+$currentDate = date('Y-m-d');
+    $rank = "FREE";
+    $expiryDate = "0";
 
+    $paidUsers = file('Database/paid.txt', FILE_IGNORE_NEW_LINES);
+    $freeUsers = file('Database/free.txt', FILE_IGNORE_NEW_LINES);
+    $owners = file('Database/owner.txt', FILE_IGNORE_NEW_LINES);
+
+    if(in_array($userId, $owners)) {
+        $rank = "OWNER";
+       $expiryDate = "UNTIL DEAD"; 
+    } else { 
+        foreach ($paidUsers as $index => $line) {
+            list($userIdFromFile, $userExpiryDate) = explode(" ", $line);
+            if ($userIdFromFile == $userId) {
+                if ($userExpiryDate < $currentDate) {
+                    unset($paidUsers[$index]); //
+                    file_put_contents('Database/paid.txt', implode("\n", $paidUsers));
+                    $freeUsers[] = $userId; // add to free users list
+                    file_put_contents('Database/free.txt', implode("\n", $freeUsers));
+                } else    $currentDate = date('Y-m-d');
+    $rank = "FREE";
+    $expiryDate = "0";
+
+    $paidUsers = file('Database/paid.txt', FILE_IGNORE_NEW_LINES);
+    $freeUsers = file('Database/free.txt', FILE_IGNORE_NEW_LINES);
+    $owners = file('Database/owner.txt', FILE_IGNORE_NEW_LINES);
+
+    if(in_array($userId, $owners)) {
+        $rank = "OWNER";
+       $expiryDate = "UNTIL DEAD"; 
+    } else {
+        foreach ($paidUsers as $index => $line) {
+            list($userIdFromFile, $userExpiryDate) = explode(" ", $line);
+            if ($userIdFromFile == $userId) {
+                if ($userExpiryDate < $currentDate) {
+                    unset($paidUsers[$index]); 
+                    file_put_contents('Database/paid.txt', implode("\n", $paidUsers));
+                    $freeUsers[] = $userId; 
+                    file_put_contents('Database/free.txt', implode("\n", $freeUsers));
+                } else {
+                    $rank = "PAID";
+                    $expiryDate = $userExpiryDate;
+                }
+            }
+        }
+    } {
+                    $rank = "PAID";
+                    $expiryDate = $userExpiryDate;
+                }
+            }
+        }
+    }
+//=======RANK DETERMINE END=========//
 $update = json_decode(file_get_contents("php://input"), TRUE);
 $text = $update["message"]["text"];
 //========WHO CAN CHECK FUNC========//
